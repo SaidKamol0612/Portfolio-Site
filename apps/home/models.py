d07df35b7eb346c13711f51d8688b23d6d265cfb
Saidkamol <kamolsaid2006@gmail.com>
2025-01-29 00:01:34 +0500

    write models and intigrate to home and index.html

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class AuthorInfo(models.Model):
+    full_name = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    phrase = models.CharField(max_length=100)
+    desc = models.TextField()
+    
+    twitter = models.CharField(max_length=100)
+    facebook = models.CharField(max_length=100)
+    telegram = models.CharField(max_length=100)
+    
+    img_1 = models.ImageField(upload_to='images/home/')
+    img_2 = models.ImageField(upload_to='images/home/')
+
+    logo = models.ImageField(upload_to='images/home/', default='logo')
+    
+    def __str__(self):
+        return self.full_name
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from .models import AuthorInfo
+
 def home_view(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context={
+        'author': AuthorInfo.objects.first(),
+    })
